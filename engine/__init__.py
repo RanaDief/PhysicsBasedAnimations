836fd394b4d8e7af5133317823cefcb407e9b6d1
you@example.com <s-rana.dief@zewailcity.edu.eg>
2026-04-23 21:57:11 +0200

    feat(collisions): add soft body particle collision demo

--- engine/__init__.py
+++ engine/__init__.py
@@ -7,11 +7,16 @@ from .collision import (
     resolve_bounds_collision,
     resolve_circle_collision,
 )
+from .particle import Particle, SoftBody
+from .spring import Spring
 
 __all__ = [
     "Body",
     "Bounds",
     "CollisionManifold",
+    "Particle",
+    "SoftBody",
+    "Spring",
     "VectorLike",
     "detect_circle_collision",
     "resolve_all_circle_collisions",
